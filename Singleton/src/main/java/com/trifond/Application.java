951a56f8d257a6d0160adb05d9df23b7b5fc0f5b
trifond <trifond@outlook.com>
2022-10-28 11:04:25 +0800

    Init project

---
+++ Singleton/src/main/java/com/trifond/Application.java
@@ -0,0 +1,16 @@
+package com.trifond;
+
+/*
+ * @ Author: Felix
+ * @ Date: 2022/10/27 22:41
+ * @ Description:
+ */
+
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
---
+++ Singleton/src/main/java/com/trifond/pattern/Singleton.java
@@ -0,0 +1,12 @@
+package com.trifond.pattern;
+
+/*
+ * @ Author: Felix
+ * @ Date: 2022/10/27 22:41
+ * @ Description:
+ */
+
+public class Singleton {
+
+
+}
