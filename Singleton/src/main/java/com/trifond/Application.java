95b36ad89921beb36b542b27d21168b2ef1c4f66
trifond <trifond@outlook.com>
2022-10-28 12:20:34 +0800

    Update singleton module

--- Singleton/src/main/java/com/trifond/Application.java
+++ Singleton/src/main/java/com/trifond/Application.java
@@ -6,9 +6,16 @@ package com.trifond;
  * @ Description:
  */
 
+import com.trifond.pattern.Singleton;
+
 public class Application {
 
     public static void main(String[] args) {
+
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+
+        System.out.println(singleton1 == singleton2);
 
     }
 
